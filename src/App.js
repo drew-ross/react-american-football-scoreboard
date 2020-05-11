017070987193f7b884748674922fa2a0fdc79a5c
Drew Ross <mail.drew.ross@gmail.com>
2020-05-11 14:57:54 -0700

    Make basic working timer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, { useState } from "react";
 import BottomRow from "./BottomRow";
+import Timer from "./Timer";
 import "./App.css";
 
 function App() {
@@ -31,7 +32,7 @@ function App() {
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <Timer />
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
